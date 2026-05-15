8268f84ca924070f799c855b24b4ff5c31e5b9b8
PepperBR <joaomarcelopimenta70@gmail.com>
2026-05-15 10:04:12 -0300

    clang

--- include/config/config.h
+++ include/config/config.h
@@ -2,23 +2,21 @@
 
 #include <string>
 
-inline std::string get_env(const std::string &key, const std::string &default_value)
-{
-    const char *value = std::getenv(key.c_str());
-    return value ? value : default_value;
+inline std::string get_env(const std::string &key,
+                           const std::string &default_value) {
+  const char *value = std::getenv(key.c_str());
+  return value ? value : default_value;
 }
 
-struct Config
-{
-    std::string host;
-    std::string port;
+struct Config {
+  std::string host;
+  std::string port;
 
-    static Config New()
-    {
-        Config config;
-        config.host = get_env("GRPC_HOST", "127.0.0.1");
-        config.port = get_env("GRPC_PORT", "50055");
+  static Config New() {
+    Config config;
+    config.host = get_env("GRPC_HOST", "127.0.0.1");
+    config.port = get_env("GRPC_PORT", "50055");
 
-        return config;
-    }
+    return config;
+  }
 };
