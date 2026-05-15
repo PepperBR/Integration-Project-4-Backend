fe103e19368b62216820d7c37b82e8bb6f7a6078
PepperBR <joaomarcelopimenta70@gmail.com>
2026-05-15 10:04:51 -0300

    commint

--- include/client/client.h
+++ include/client/client.h
@@ -9,19 +9,20 @@
 
 using ResponseList = std::vector<std::vector<std::string>>;
 using Response = std::vector<std::string>;
-class MeterClient {
+class MeterClient
+{
 public:
-  MeterClient(std::shared_ptr<grpc::Channel> channel);
+    MeterClient(std::shared_ptr<grpc::Channel> channel);
 
-  auto AddMeter(const std::string &meter_id) -> Response;
-  auto ListAllCreatedMeters() -> ResponseList;
-  auto ListAllAvailableMeters() -> ResponseList;
-  auto GetMeasurements(const std::string &meter_id) -> Response;
-  auto ListLines() -> Response;
-  auto ListCreatedMeters(const std::string &line_name) -> ResponseList;
-  auto ListAvailableMeters(const std::string &line_name) -> ResponseList;
-  auto RemoveMeter(const std::string &meter_id) -> std::string;
+    auto AddMeter(const std::string &meter_id) -> Response;
+    auto ListAllCreatedMeters() -> ResponseList;
+    auto ListAllAvailableMeters() -> ResponseList;
+    auto GetMeasurements(const std::string &meter_id) -> Response;
+    auto ListLines() -> Response;
+    auto ListCreatedMeters(const std::string &line_name) -> ResponseList;
+    auto ListAvailableMeters(const std::string &line_name) -> ResponseList;
+    auto RemoveMeter(const std::string &meter_id) -> std::string;
 
 private:
-  std::unique_ptr<meter::v1::MeterService::Stub> stub_;
+    std::unique_ptr<meter::v1::MeterService::Stub> stub_;
 };
